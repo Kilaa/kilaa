dec81eb0412cb993d8cf3efbc5750db12396a684
Michel Phuong <michelphuong@hotmail.fr>
2024-01-09 19:38:47 +0100

    Mise en place du carrousel ainsi que de son defilement infinie

--- assets/script.js
+++ assets/script.js
@@ -35,15 +35,59 @@ const arrowRight = document.querySelector('#arrow_right');
 arrowLeft.addEventListener('click', function() {
     console.log('Clic sur la flèche gauche');
     compteur -= 1;
-	if(0>compteur){compteur=slides.length-1;}
-	console.log(compteur)
+	if 	(compteur < 0) 
+		{compteur = slides.length - 1;}
+		updateBanner(); // Appel de la fonction pour mettre à jour le banner après le changement de slide
 });
+
+// Sélectionnez les bullet points
+const dotsContainer = document.getElementById('dots');
+const dots = document.querySelectorAll('.dot');
+
 
 // event listener flèche droite
 arrowRight.addEventListener('click', function() {
     console.log('Clic sur la flèche droite');
     compteur += 1;
-	if(slides.length-1 < compteur){compteur=0;}
-	console.log(compteur)
+	 // Si on est à la dernière image, passe à la première
+	 if (compteur >= slides.length)
+	  	{compteur = 0;}
+
+		updateBanner(); // Appel de la fonction pour mettre à jour le banner après le changement de slide
 });
+
+// Fonction pour mettre à jour les bullet points
+function updateBullets() {
+    dots.forEach((dot, index) => {
+        if (index === compteur) {
+            dot.classList.add('dot_selected');
+        } else {
+            dot.classList.remove('dot_selected');
+        }
+    });
+}
+
+// Fonction pour mettre à jour le banner (image, tagLine, bullet points)
+function updateBanner() {
+    const bannerImage = document.querySelector('.banner-img');
+    const tagLine = document.querySelector('#banner p');
+	
+
+// Assurez que l'index est valide
+    if (compteur < 0) {
+        compteur = slides.length - 1;
+    } else if (compteur >= slides.length) {
+        compteur = 0;
+    }
+
+// Pour mettre à jour l'image et le texte
+    bannerImage.src = `./assets/images/slideshow/${slides[compteur].image}`;
+    tagLine.innerHTML = slides[compteur].tagLine;
+
+// Mettre à jour les bullet points
+    updateBullets();
+}
+
+// Metttre à jour le banner lors du chargement initial
+updateBanner();
 
